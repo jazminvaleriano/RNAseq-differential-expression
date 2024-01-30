3b49cb39c16f32caf54efd4e9c5fc1728937940e
jazminvaleriano <jazmin.valeriano@gmail.com>
2024-01-30 14:31:53 +0100

    changed logic for transcript to gene

--- scripts/06_Transcript_to_Gene.sh
+++ scripts/06_Transcript_to_Gene.sh
@@ -18,12 +18,15 @@ awk '$1 ~ /ch/ && $3 == "transcript" {print $12 "\t" $10 "\t" $14}' $GTF_FILE | 
 # Create a table with biotypes retrieved from the reference GTF file.
 # Extract columns 12 and 14, remove semicolons, and use tabs as separators
 echo -e 'transcript_id\tbiotype' > $OUTPUT_DIR/biotype.tsv
-awk '$3 == "transcript" {print $12 "\t" $14}' $REFERENCE_GTF | sed 's/;//g' | sort >> $OUTPUT_DIR/biotype.tsv
+awk '$3 == "transcript" {print $12 "\t" $14}' $REFERENCE_GTF | sed 's/;//g'| sort >> $OUTPUT_DIR/biotype.tsv
 
 # Join both tables (add gene_type as the 4th column from the reference genome GTF)
-join --nocheck-order -t $'\t' -a 1 -a 2 -1 1 -2 1 $OUTPUT_DIR/transcripts.tsv $OUTPUT_DIR/biotype.tsv > $OUTPUT_DIR/transcript_to_gene.tsv
+join --nocheck-order -t $'\t' -a 1 -a 2 -1 1 -2 1 $OUTPUT_DIR/transcripts.tsv $OUTPUT_DIR/biotype.tsv | sed 's/[[:blank:]]*$//'| tr -s ' ' > $OUTPUT_DIR/transcript_to_gene.tsv
 
-rm transcripts.tsv biotype.tsv
+# If the transcript was not associated to an annotated gene, the transcript id will be re assigned as gene id and name, under the assumption 
+# that this is not an isoform, but a novel gene, and the biotype will be assigned as "novel"
+awk 'BEGIN {OFS=FS="\t"} NF==2 {print $1, $1, $1, "\"novel\"" } NF!=2 {print $0}' $OUTPUT_DIR/transcript_to_gene.tsv > tmp && mv tmp $OUTPUT_DIR/transcript_to_gene.tsv
+rm $OUTPUT_DIR/transcripts.tsv $OUTPUT_DIR/biotype.tsv
 
 
 
--- scripts/16_summary.sh
+++ scripts/16_summary.sh
@@ -17,8 +17,10 @@ tail -n +2 $FILE_DIF_EXP | awk -F ',' '{print $1, $2, $5, $6, $7}' | tr -s ' ' |
 awk -F '\t' '{print $4, $1, $2, $3, $6}' $FILE_NOVEL_COORDINATES |tr ' "' '\t' | sort -n > sorted.coords.tmp
 tr '::' '\t' < $FILE_CPAT | awk -F '\t' '{print $1 "\t" $8}'| sort > tabbed.pcp.tmp
 
-# Join the coordinates and Sleuth results (will only conserve the transcripts included in Sleuth analysis)
-join --nocheck-order -t$'\t' sorted.coords.tmp summary.de.tmp  -a 2 | sed 's/ $//' > summary.tsv
+# Join the coordinates and Sleuth results (will also conserve the transcripts not included in Sleuth analysis)
+join --nocheck-order -t$'\t' sorted.coords.tmp summary.de.tmp  -a 1 | sed 's/ $//' > summary.tsv
+awk 'BEGIN {OFS=FS="\t"} NF==5 {print $0, "NA", "NA", "NA", "NA"} NF!=5 {print $0}' summary.tsv > temp && mv temp summary.tsv
+
 
 # Add a column to indicate if the transcrips are listed in the results for Intergenic, 3' poly A, 5' TSS
 awk -F'\t' 'NR==FNR{a[$4]=$4;next} {print $0 "\t" ($1 in a ? "intergenic" : "not_intergenic")}' $FILE_INTERGENIC summary.tsv > temp && mv temp summary.tsv
@@ -28,17 +30,18 @@ awk -F'\t' 'NR==FNR{a[$4]=$4;next} {print $0 "\t" ($1 in a ? "TSS_match" : "no_T
 # Add the protein coding potential score
 join --nocheck-order -t$'\t' summary.tsv tabbed.pcp.tmp > temp && mv temp summary.tsv
 
-# Add heder row
-echo -e "target_id\tchr\tstart\tend\tstrand\tgene_id\tDE_pval\tDE_qval\tlog2fold_change\tintergenic\tPoly_A_match\tTSS_match\tcoding_potential" | cat - summary.tsv > temp && mv temp summary.tsv
-
 # Extract the top lncRNA candidates
 # NR==1 to leave the header out, filter by intergenic, polya and tss match, low protein coding score, and sort by column 8 (DE q value)
-awk 'NR==1 || ($10=="intergenic" && $11=="poly_a_match" && $12=="TSS_match" && $13<0.364 && $7<=0.01)' summary.tsv | sort -t$'\t' -k8n > ${OUTPUT_DIR}/lncRNA_candidates.tsv
+awk '$10=="intergenic" && $11=="poly_a_match" && $12=="TSS_match" && $13<0.364' summary.tsv | sort -t$'\t' -k8n > lncRNA_candidates.tsv
 
+# Add heder row to both files 
+echo -e "target_id\tchr\tstart\tend\tstrand\tgene_id\tDE_pval\tDE_qval\tlog2fold_change\tintergenic\tPoly_A_match\tTSS_match\tcoding_potential" | cat - summary.tsv > temp && mv temp summary.tsv
+echo -e "target_id\tchr\tstart\tend\tstrand\tgene_id\tDE_pval\tDE_qval\tlog2fold_change\tintergenic\tPoly_A_match\tTSS_match\tcoding_potential" | cat - lncRNA_candidates.tsv > temp && mv temp lncRNA_candidates.tsv
 
 # Remove temporary files and save summary in output folder
 rm *tmp
 mv summary.tsv ${OUTPUT_DIR}
+mv lncRNA_candidates.tsv ${OUTPUT_DIR}
 
 
 
